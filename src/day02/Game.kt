b48c6cad2e86023be3a0545384919235ebe55969
Gabriel Brand <gabr.brand@gmail.com>
2023-12-03 11:12:32 +0100

    Revert "Move ```Game``` and ```Set``` to separate file"
    This reverts commit 8d5a045a67ce8b2349d63725cd34371c09898faf.

--- src/day02/Game.kt
+++
@@ -1,6 +0,0 @@
-package day02
-
-data class Game(
-    val id: Int,
-    val sets: List<Set>
-)
--- src/day02/Set.kt
+++
@@ -1,7 +0,0 @@
-package day02
-
-data class Set(
-    var redCubes: Int = 0,
-    var greenCubes: Int = 0,
-    var blueCubes: Int = 0,
-)
