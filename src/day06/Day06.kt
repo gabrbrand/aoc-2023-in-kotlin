c8f984157ec122c146a954b7b29e40df8b8a2c95
Gabriel Brand <gabr.brand@gmail.com>
2023-12-06 18:59:15 +0100

    Refactor Day 6

--- src/day06/Day06.kt
+++ src/day06/Day06.kt
@@ -1,14 +1,14 @@
 package day06
 
-import kotlin.io.path.Path
-import kotlin.io.path.readLines
+import println
+import readInput
 
 fun main() {
     Day06()
 }
 
 data class Race(
-    val time: Long,
+    val timeAllowed: Long,
     val recordDistance: Long
 )
 
@@ -16,55 +16,50 @@ enum class Part { ONE, TWO }
 
 class Day06 {
     init {
-        val exampleInput = Path("src/day06/Day06_Example.txt").readLines()
-        val exampleRacesPart1 = exampleInput.parse(Part.ONE)
-        check(part1(exampleRacesPart1) == 288)
-        val exampleRacePart2 = exampleInput.parse(Part.TWO).single()
-        check(part2(exampleRacePart2) == 71503)
+        val exampleInput = readInput("day06/Day06_Example")
+        val exampleRaces = exampleInput.parse(Part.ONE)
+        check(part1(exampleRaces) == 288)
+        val exampleRace = exampleInput.parse(Part.TWO).single()
+        check(part2(exampleRace) == 71503)
 
-        val input = Path("src/day06/Day06.txt").readLines()
-        val racesPart1 = input.parse(Part.ONE)
-        println(part1(racesPart1))
-        val racePart2 = input.parse(Part.TWO).single()
-        println(part2(racePart2))
+        val input = readInput("day06/Day06")
+        val races = input.parse(Part.ONE)
+        part1(races).println()
+        val race = input.parse(Part.TWO).single()
+        part2(race).println()
     }
 
     private fun List<String>.parse(part: Part): List<Race> {
-        val races = mutableListOf<Race>()
-        if (part == Part.ONE){
-            val times = first()
+        if (part == Part.ONE) {
+            val timesAllowed = first()
                 .removePrefix("Time:")
                 .split(" ")
                 .filterNot { element -> element.isBlank() }
                 .map { time -> time.toLong() }
-            val distances = last()
+            val recordDistances = last()
                 .removePrefix("Distance:")
                 .split(" ")
                 .filterNot { element -> element.isBlank() }
                 .map { time -> time.toLong() }
 
-            for (i in 0..times.lastIndex) {
-                races.add(
-                    Race(
-                        time = times[i],
-                        recordDistance = distances[i]
-                    )
+            return (timesAllowed zip recordDistances).map { (timeAllowed, recordDistance) ->
+                Race(
+                    timeAllowed = timeAllowed,
+                    recordDistance = recordDistance
                 )
             }
         } else {
-            races.add(
+            return listOf(
                 Race(
-                    time = first().filter { element -> element.isDigit() }.toLong(),
-                    recordDistance = last().filter { element -> element.isDigit() }.toLong()
+                    timeAllowed = first().filter { character -> character.isDigit() }.toLong(),
+                    recordDistance = last().filter { character -> character.isDigit() }.toLong()
                 )
             )
         }
-
-        return races
     }
 
     private val Race.winningOptions: Int
-        get() = (1..<time).count { startingSpeed -> (time - startingSpeed) * startingSpeed > recordDistance }
+        get() = (1..<timeAllowed).count { startingSpeed -> (timeAllowed - startingSpeed) * startingSpeed > recordDistance }
 
     private fun part1(races: List<Race>) = races.fold(1) { answer, race -> answer * race.winningOptions }
 
